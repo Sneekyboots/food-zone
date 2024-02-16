c24f9a6b21c070c7963dc57dbc50bff8a1a2e834
SneekyBoots <sneekyboots122@gmail.com>
2024-02-16 19:40:58 +0530

    added restaurant scren

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,10 +1,21 @@
 import { View, Text, TouchableWithoutFeedback ,Image} from 'react-native'
 import React from 'react'
+import * as Icon from "react-native-feather";
+import {themecolors} from "../theme"
+import {useNavigation} from "@react-navigation/native"
+
 
 export default function RestaurantCard({item}) {
+  const navigation=useNavigation()
   return (
-    <TouchableWithoutFeedback>
-        <View className="mr-6 bg-white rounded-3xl shadow-lg">
+    <TouchableWithoutFeedback
+    onPress={() => navigation.navigate("Restaurant", {...item})}>
+        <View 
+        style={{
+          shadowColor: themecolors.bgColor(1),
+
+        }}
+        className="mr-6 bg-white rounded-3xl shadow-lg">
         <Image className="h-36 w-64 rounded-t-3xl"
          source={item.image}/>
          <View className="p-3 pb-4 space-y-2">
@@ -13,10 +24,15 @@ export default function RestaurantCard({item}) {
                 <Image source={require("../assets/images/fullStar.png")} className="h-4 w-4"/>
                 <Text className="text-xs">
                   <Text className="text-green-700">{item.stars}</Text>
-                  <Text className="text-gray-700"> ({item.reviews} review)</Text> · <Text className="font-semibold text-gray-700">{item.category}</Text>
+                  <Text className="text-gray-700"> ({item.reviews} review)</Text> · <Text className="font-semibold text-gray-700">{item.category}
+                  </Text>
               </Text>
             </View>
+            <View className="flex-row items-center space-x-1">
+                <Icon.MapPin height="15" width="15" stroke="gray"/>
+                <Text className="text-gray-600 text-xs">Nearby · {item.address}</Text>
          </View>
+        </View>
         </View>
     </TouchableWithoutFeedback>
   )
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,7 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native';
 
 export default function RestaurantScreen() {
+  const {params}=useRoute();
+  let item=params;
+  console.log('restaurant:',item)
+  console.log(params)
   return (
     <View>
       <Text>RestaurantScreen</Text>
